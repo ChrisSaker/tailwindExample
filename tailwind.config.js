9fa3ecdbf052598a3b33d3b7f29bd03b084d0373
Chris Saker <chris.saker@endspacemena.com>
2024-06-26 16:46:13 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './src/*.{html,js}', // Adjust the paths based on your project structure
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem'
+    },
+  },
+  plugins: [],
+};
